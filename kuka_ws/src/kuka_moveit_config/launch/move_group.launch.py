c14d0e7fe6c535d7cc6880df8fcf376b923b31bf
Junjie Yu <1282188778@qq.com>
2026-04-08 20:15:50 +0800

    Path Planning Demo

--- kuka_ws/src/kuka_moveit_config/launch/move_group.launch.py
+++ kuka_ws/src/kuka_moveit_config/launch/move_group.launch.py
@@ -3,5 +3,8 @@ from moveit_configs_utils.launches import generate_move_group_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("assembly_moveit", package_name="kuka_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("assembly_moveit", package_name="kuka_moveit_config").planning_pipelines(
+        pipelines=["ompl", "pilz_industrial_motion_planner"],
+        default_planning_pipeline="ompl"
+    ).to_moveit_configs()
     return generate_move_group_launch(moveit_config)
